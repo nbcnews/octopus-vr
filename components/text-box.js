4870fdc496b899352156d685faceeba4c5fbb1e8
Xinran Wang <Xinran_wang@comcast.com>
2017-10-15 11:10:12 -0400

    Add news ticker to main room

--- components/text-box.js
+++ components/text-box.js
@@ -1,11 +1,25 @@
 AFRAME.registerComponent('text-box', {
+  schema: {
+    textEl: {type: 'string', default: '#newsDescriptionText'},
+  },
   init: function() {
     console.log('in init text-box');
   },
 
-  toggle: function() {
-    const el = this.el;
-    console.log('hide');
-    el.setAttribute('visible', !el.getAttribute('visible'));
+  play: function(){
+    const data = [
+      'Airport Bomb raises fears about security',
+      'Micheal Estes: "fight a war on U.S. soil"',
+      'Up Next: Law Enforcement Analyst, Jim Cavanaugh',
+      'Next: Attack at baggage terminal'
+    ];
+
+    const newsTextEl = document.querySelector(this.data.textEl);
+
+    let counter = 0
+    const timer = setInterval(() => {
+      counter = (counter++) % data.length
+      newsTextEl.setAttribute('value', data[counter])
+    }, 3000)
   }
 });
